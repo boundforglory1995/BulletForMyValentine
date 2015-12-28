94f4f1fe77cac94552799f5da812316e5651dec7
Gianni <luciferdeveloper@gmail.com>
2015-12-28 17:13:48 +0100

    version1

---
+++ index.php
@@ -0,0 +1,149 @@
+<?php 
+include('views/head.php');
+
+?>
+
+	<section class="billboard dark">
+		<header class="wrapper dark">
+			
+			<nav>
+				<ul>
+					<li><a href="">Music</a></li>
+					<li><a href="">Pictures</a></li>
+					<li><a href="">News</a></li>
+					<li><a href="">Facebook</a></li>
+				</ul>
+			</nav>
+		</header>
+
+		<div class="caption dark animated wow fadeInDown clearfix">
+			<h1 id="bullet">Bullet For My Valentine</h1>
+			
+			
+		</div>
+		
+	</section><!--  End billboard  -->
+
+
+	<section class="services wrapper">
+		<ul class="clearfix">
+			<li class="animated wow fadeInDown">
+				<img class="icon" src="img/icon1.png" alt=""/>
+				<span class="separator"></span>
+				<h2>Matthew Tuck</h2>
+				<p>Geboren: 20 januari 1980 (35 jaar), Bridgend, Verenigd Koninkrijk
+Muziekgroepen: Bullet For My Valentine (sinds 1998), AxeWound (sinds 2011)</p>
+			</li>
+			<li class="animated wow fadeInDown"  data-wow-delay=".2s">
+				<img class="icon" src="img/icon2.png" alt=""/>
+				<span class="separator"></span>
+				<h2>Michael Paget</h2>
+				<p>Geboren: 12 september 1978 (37 jaar), Bridgend, Verenigd Koninkrijk
+Muziekgroep: Bullet For My Valentine (sinds 1998)</p>
+			</li>
+			<li class="animated wow fadeInDown"  data-wow-delay=".4s">
+				<img class="icon" src="img/icon3.png" alt=""/>
+				<span class="separator"></span>
+				<h2>Michael Thomas</h2>
+				<p>Geboren: 4 juni 1981 (34 jaar), Bridgend, Verenigd Koninkrijk
+Muziekgroep: Bullet For My Valentine (sinds 1998)</p>
+			</li>
+			<li class="animated wow fadeInDown"  data-wow-delay=".4s">
+				<img class="icon" src="img/icon4.png" alt=""/>
+				<span class="separator"></span>
+				<h2>Jason James</h2>
+				<p>Geboren: 13 januari 1981 (34 jaar), Bridgend, Verenigd Koninkrijk
+Platenlabel: Roadrunner Records
+Muziekgroep: Bullet For My Valentine (2003 – 2015)</p>
+			</li>
+		</ul>
+	</section><!--  End services  -->
+
+
+	<section class="video">
+	
+	</section><!--  End video  -->
+
+
+	<section class="testimonials wrapper">
+		<div class="title animated wow fadeIn">
+			<h2>Albums</h2>
+			</div>
+
+		
+			<ul class="clearfix">
+			<li class="animated wow fadeInDown">
+				<img class="icon" src="img/thepoison.jpg" alt=""/>
+				<span class="separator"></span>
+				<h2>The Poison</h2>
+				<p>The Poison is the debut album by Welsh heavy metal band Bullet for My Valentine. The album was released on 3 October 2005 through Visible Noise Records in the UK, and on February 14, 2006 in the United States through Trustkill Records.</p>
+			</li>
+			<li class="animated wow fadeInDown"  data-wow-delay=".2s">
+				<img class="icon" src="img/scream.jpg" alt=""/>
+				<span class="separator"></span>
+				<h2>Scream Aim Fire</h2>
+				<p>Scream Aim Fire is the second studio album by Welsh heavy metal band Bullet for My Valentine, released on 28 January 2008 in the UK and the preceding day in the US through Jive Records. Since its release, Scream Aim Fire has sold over 2 million copies worldwide, with 500,000 copies being sold in the US alone.</p>
+			</li>
+			<li class="animated wow fadeInDown"  data-wow-delay=".4s">
+				<img class="icon" src="img/fever.jpg" alt=""/>
+				<span class="separator"></span>
+				<h2>Fever</h2>
+				<p>Fever is the third studio album by Welsh heavy metal band, Bullet for My Valentine. Containing eleven tracks,[1] the album was released on 26 and 27 April 2010 in the UK and in the US, respectively.)</p>
+			</li>
+			<li class="animated wow fadeInDown"  data-wow-delay=".4s">
+				<img class="icon" src="img/temper.jpg" alt=""/>
+				<span class="separator"></span>
+				<h2>Temper Temper</h2>
+				<p>Temper Temper is the fourth studio album by the Welsh heavy metal band Bullet for My Valentine released on 8 February 2013 in Australia, and 11 February 2013 worldwide under RCA Records, their first under the label</p>
+			</li>
+			<li class="animated wow fadeInDown"  data-wow-delay=".4s">
+				<img class="icon" src="img/venom.jpg" alt=""/>
+				<span class="separator"></span>
+				<h2>Venom</h2>
+				<p>Venom is the fifth studio album by the Welsh heavy metal band Bullet for My Valentine, released on 14 August 2015. It is the first album by the band since the departure of bassist Jason "Jay" James, which was announced in February 2015 while the band was recording the album.</p>
+			</li>
+		</ul>
+	</section><!--  End testimonials  -->
+
+	<section class="testimonials wrapper">
+		<div class="title animated wow fadeIn">
+			<h2>Band Quotes</h2>
+			</div>
+
+		
+			<ul class="clearfix">
+			<li class="animated wow fadeInDown">
+				
+				
+				<p>Cold sweats, hallucinations I wanna speed to stop. The hell I'm going through The addiction's taking you.</p>
+			</li>
+			<li class="animated wow fadeInDown"  data-wow-delay=".2s">
+				
+			
+				<p>What's happening to me!? I'm dying from the inside! Body hurts too much to feel! Pressure adds to pain! Deliver us from evil! Straying out of sanity!.</p>
+			</li>
+			<li class="animated wow fadeInDown"  data-wow-delay=".4s">
+				
+				
+				<p>Fever is the third studio album by Welsh heavy metal band, Bullet for My Valentine. Containing eleven tracks,[1] the album was released on 26 and 27 April 2010 in the UK and in the US, respectively.)</p>
+			</li>
+			<li class="animated wow fadeInDown"  data-wow-delay=".4s">
+				
+				
+				<p>Can you see me through bloodshot eyes? Should I fight for what is right or let it die? Now I'm choking on force fed lies do I fight or let it die?</p>
+			</li>
+			<li class="animated wow fadeInDown"  data-wow-delay=".4s">
+				
+				
+				<p>Torn apart at the seams and my dreams turn to tears, I'm not feeling this situation. Run away try to find a safe place you can hide It's the best place to be when you're feeling like..</p>
+			</li>
+		</ul>
+	</section><!--  End testimonials  -->
+
+
+		
+	
+
+
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+	//fancybox.js init
+	$('.fancybox').fancybox({
+		openEffect : 'none',
+		closeEffect : 'none',
+		prevEffect : 'none',
+		nextEffect : 'none',
+
+		arrows : false,
+		helpers : {
+			media : {},
+			buttons : {}
+		}
+	});
+
+
+
+	//wow.js init
+	wow = new WOW(
+	    {
+		  animateClass: 'animated',
+		  mobile: false,
+		  offset: 100
+		}
+	);
+	wow.init();
+
+			var currentPosition = 0;
+		var slideWidth = 500;
+		var slides = $('.slide');
+		var numberOfSlides = slides.length;
+		var slideShowInterval;
+		var speed = 3000;
+
+		
+		slideShowInterval = setInterval(changePosition, speed);
+		
+		slides.wrapAll('<div id="slidesHolder"></div>')
+		
+		slides.css({ 'float' : 'left' });
+		
+		$('#slidesHolder').css('width', slideWidth * numberOfSlides);
+		
+		
+		function changePosition() {
+			if(currentPosition == numberOfSlides - 1) {
+				currentPosition = 0;
+			} else {
+				currentPosition++;
+			}
+			moveSlide();
+		}
+		
+		
+		function moveSlide() {
+				$('#slidesHolder')
+				  .animate({'marginLeft' : slideWidth*(-currentPosition)});
+}
+
+
+
+
+});
+
+
+
+							   
